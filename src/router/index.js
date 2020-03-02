66d9851beabe384270a6fa62453f23cbbc8d31b1
Maksym Hryhoriev <zercos2106@gmail.com>
2020-03-02 21:44:01 +0100

    Add pages templates and register them in router

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,59 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { layout: 'main' },
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'empty' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: { layout: 'main' },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    meta: { layout: 'main' },
+    component: () => import('../views/Detail.vue')
   }
 ]
 
